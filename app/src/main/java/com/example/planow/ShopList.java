5b4d2fe154ab962b9be82938233f5d3a6ce2d77a
Dubovec <dubovec95@ukr.net>
2019-04-19 12:30:11 +0300

    made ShopList and WishList

--- app/src/main/java/com/example/planow/ShopList.java
+++ app/src/main/java/com/example/planow/ShopList.java
@@ -1,15 +1,85 @@
 package com.example.planow;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class ShopList extends Activity {
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
-    public void onCreate(Bundle savedInstanceState) {
+public class ShopList extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_list);
 
+        lvItems = findViewById(R.id.lvItems);
+        items = new ArrayList<>();
+        readItems();
+        itemsAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
 
+        setupListViewListener();
+
+    }
+
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        // Remove the item within array at position
+                        items.remove(pos);
+                        // Refresh the adapter
+                        itemsAdapter.notifyDataSetChanged();
+                        // Return true consumes the long click event (marks it handled)
+                        writeItems();
+                        return true;
+                    }
+                });
+    }
+
+    public void onAddItem(View v) {
+        Toast.makeText(this, "Натисніть та утримуйте на продукті для видалення", Toast.LENGTH_SHORT).show();
+        EditText etNewItem = findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File shopFile = new File(filesDir, "shop.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(shopFile));
+        } catch (IOException e) {
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File shopFile = new File(filesDir, "shop.txt");
+        try {
+            FileUtils.writeLines(shopFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
