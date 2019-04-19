931a4c564a4305b889621f9e075e2de2bf3c2d32
Dubovec <dubovec95@ukr.net>
2019-04-19 18:27:43 +0300

    connect AWS to project

--- app/src/main/java/com/example/planow/MainActivity.java
+++ app/src/main/java/com/example/planow/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.planow;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
+
+
 public class MainActivity extends AppCompatActivity {
-
-
 
     public Button todolist, shoplist, trawellist, wishlist;
 
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(todo);
             }
         });
+
     }
 
     public void gototrawel(){
@@ -70,6 +75,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
+            }
+        }).execute();
 
         gotodo();
         gototrawel();
